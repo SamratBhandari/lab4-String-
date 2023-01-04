15993ded8b08e9564cc4cf82da644fc8877920ad
SamratBhandari <samratbhandari2000@gmail.com>
2023-01-04 09:29:59 +0545

    Update the code to modify the starting position of the sub string

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿string message = "Find what is (inside the parentheses)";
 int openingPosition = message.IndexOf('(');
 int closingPosition = message.IndexOf(')');
-// Console.WriteLine(openingPosition);
-// Console.WriteLine(closingPosition);
+openingPosition += 1;
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length));

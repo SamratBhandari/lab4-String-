825a0dea99ab8993baebcb19f996d79a5f14b88e
SamratBhandari <samratbhandari2000@gmail.com>
2023-01-04 10:01:14 +0545

    Solution

--- Program.cs
+++ Program.cs
@@ -1,4 +1,35 @@
-﻿string message = "This--is--ex-amp-le--da-ta";
-message = message.Replace("--", " ");
-message = message.Replace("-", "");
-Console.WriteLine(message);
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+
+
+const string spanTag = "<span>";
+
+
+int quantityStart = input.IndexOf(spanTag);
+int quantityEnd = input.IndexOf("</span>");
+quantityStart += spanTag.Length;
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+
+
+output = input.Replace("&trade;", "&reg;");
+
+
+int divStart = input.IndexOf("<div");
+int divEnd = input.IndexOf(">");
+int divLength = divEnd - divStart;
+divLength += 1;
+output = output.Remove(divStart, divLength);
+
+
+int divCloseStart = output.IndexOf("</div");
+int divCloseEnd = output.IndexOf(">", divCloseStart);
+int divCloseLength = divCloseEnd - divCloseStart;
+divCloseLength += 1;
+output = output.Remove(divCloseStart, divCloseLength);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");

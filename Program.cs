f1567ac44c5b1f31615236cb391ff6499cb68e0d
SamratBhandari <samratbhandari2000@gmail.com>
2023-01-04 09:32:01 +0545

    Write code to retrieve the last occurrence of a sub string

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿string message = "Find what is (inside the parentheses)";
-int openingPosition = message.IndexOf('(');
-int closingPosition = message.IndexOf(')');
+﻿string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
 openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length));

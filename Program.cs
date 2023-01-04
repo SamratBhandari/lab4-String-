988313c2c9face5c766028a090c7b066366da969
SamratBhandari <samratbhandari2000@gmail.com>
2023-01-04 09:34:13 +0545

    Update the code example to retrieve any value between one or more sets of parentheses in a string

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿string message = "(What if) I am (only interested) in the last (set of parentheses)?";
-int openingPosition = message.LastIndexOf('(');
-openingPosition += 1;
-int closingPosition = message.LastIndexOf(')');
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+﻿string message = "(What if) there are (more than) one (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1) break;
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+    message = message.Substring(closingPosition + 1);
+}

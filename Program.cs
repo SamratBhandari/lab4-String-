db1197ec42b61b5f706d572692a3ab33f4723b76
SamratBhandari <samratbhandari2000@gmail.com>
2023-01-04 09:58:10 +0545

    Use of remove and replace methods

--- Program.cs
+++ Program.cs
@@ -1,44 +1,4 @@
-﻿string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
-
-
-
-char[] openSymbols = { '[', '{', '(' };
-
-
-
-int closingPosition = 0;
-
-while (true)
-{
-    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
-
-    if (openingPosition == -1) break;
-
-    string currentSymbol = message.Substring(openingPosition, 1);
-
-    
-    char matchingSymbol = ' ';
-
-    switch (currentSymbol)
-    {
-        case "[":
-            matchingSymbol = ']';
-            break;
-        case "{":
-            matchingSymbol = '}';
-            break;
-        case "(":
-            matchingSymbol = ')';
-            break;
-    }
-
-   
-    
-    openingPosition += 1;
-    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
-
-    
-
-    int length = closingPosition - openingPosition;
-    Console.WriteLine(message.Substring(openingPosition, length));
-}
+﻿string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+Console.WriteLine(message);
